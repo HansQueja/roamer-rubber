733548c6cd23cd97fa73a1244287f2b4a53a063d
Hans Queja <hansqueja8@gmail.com>
2026-06-28 04:18:12 +0800

    feat: Test deep enhanced cnn segmentation

--- src/model.py
+++ src/model.py
@@ -93,16 +93,10 @@ class DeepEnhancedCNN(nn.Module):
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
             nn.MaxPool2d(2, 2),
             
             # Block 4: 256 Filters (Double Conv)
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
